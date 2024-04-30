189e298b93f4b3e9579d1a5c4511d0b952f57fd4
Jisu-1104 <128033196+Jisu-1104@users.noreply.github.com>
2024-04-30 23:40:00 +0900

    손이 오브젝트 앞으로 오게 수정
    손 투명도라던가 그런 건 만나서 보면서 이쁘게 바꾸는 게 좋을 듯

--- Assets/WhiteRabbit/Scripts/StartScene/IsGroundVisited.cs
+++ Assets/WhiteRabbit/Scripts/StartScene/IsGroundVisited.cs
@@ -6,12 +6,25 @@ public class IsGroundVisited : MonoBehaviour
 {
     public GameObject player;
     public GameObject howtoplay;
+    public GameObject npc;
+    public PhysicsMaterial2D newMaterial;
+    private bool hasCollided = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject == player && !hasCollided)
+        {
+            npc.SetActive(true);
+            howtoplay.SetActive(true);
+            hasCollided = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
         if (collision.gameObject == player)
         {
-            howtoplay.SetActive(true);
+            // 충돌 종료 시 no bounce Material로 변경
+            player.GetComponent<BoxCollider2D>().sharedMaterial = newMaterial;
         }
     }
 }
