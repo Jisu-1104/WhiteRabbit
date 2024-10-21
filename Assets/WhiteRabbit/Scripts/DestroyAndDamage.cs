64bf9a1e9f94c3a23d25e9d0800cae0fe0c06f3c
Jisu-1104 <128033196+Jisu-1104@users.noreply.github.com>
2024-10-21 17:05:14 +0900

    소리, 운석 충돌 이펙트 추가

--- Assets/WhiteRabbit/Scripts/DestroyAndDamage.cs
+++ Assets/WhiteRabbit/Scripts/DestroyAndDamage.cs
@@ -5,6 +5,8 @@ public class DestroyAndDamage : MonoBehaviour
     public string targetTag = "suwhaObject"; // 대상 태그
     public int damageAmount = 10; // 입힐 피해량
     public PlayerHealth playerHealth; // PlayerHealth 컴포넌트
+    public AudioSource audioSource;
+    public GameObject effectPrefab;
 
     // 트리거 충돌시 호출되는 이벤트
     private void OnTriggerEnter2D(Collider2D other)
@@ -12,8 +14,10 @@ public class DestroyAndDamage : MonoBehaviour
         Debug.Log("충돌감지");
                   
         playerHealth.TakeDamage(damageAmount);
-        
-        
+        audioSource.Play();
+        Instantiate(effectPrefab, transform.position, transform.rotation);
+
+
         // 충돌한 오브젝트 파괴         
         Destroy(other.gameObject);        
     }
